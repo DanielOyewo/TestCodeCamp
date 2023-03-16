89d385c92e13f5e6b7b79140a3a00cb38c8f8cdb
DanielOyewo <oyewo.daniel05@gmail.com>
2023-03-16 19:14:04 +0100

    Added Task 14

---
+++ src TASK 14/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("My name is Daniel Oyewo.");
+        System.out.println("This is my first java project.");
+        System.out.println("I am Software Test Engineer Trainee");
+        System.out.println("I am currently a student at Testify Academy");
+        System.out.println("I worked closely with peers to identify issues and provide suitable resolutions");
+        System.out.println("I function optimally both with a team and independently.");
+        System.out.println("I wrote and optimized test cases to maximize success of manual software.");
+    }
+
+}
